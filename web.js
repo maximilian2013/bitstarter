122d11370aff8d7a1ee99f24fdfb36e2d00b0c0c
Maximilian Hörnis <maximilian@hoernis.de>
2013-07-12 00:08:47 +0200

    - Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs'); 
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+   var contents = fs.readFileSync('index.html');
+   response.end(contents);
 });
 
 var port = process.env.PORT || 5000;
