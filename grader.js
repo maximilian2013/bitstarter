3d0240f79b51f8d73dc053ebd637bd9dd2e7986a
Maximilian Hörnis <maximilian@hoernis.de>
2013-07-12 01:22:36 +0200

    - Updated grader.js

--- grader.js
+++ grader.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -24,51 +25,108 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var rest = require('restler');
+var http = require('http');
+
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
+  var instr = infile.toString();
+  if (!fs.existsSync(instr)) {
+    console.log("%s does not exist. Exiting.", instr);
+    process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+  }
+  return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertURLExists = function(inurl, callback) {
+  http.get(inurl, function(res) {
+    callback("fine");
+  }).on('error', function(e) {
+    callback("error");
+  });
+}
+
+var cheerioHtmlFile = function(htmlfile, callback) {
+  // Check whether we got an url or a file.
+  // Note that this is actually a hack cause the URL could
+  // have a different format. A simple https:// could crash
+  // the whole thing.
+  if (htmlfile.indexOf("http://") !== -1) {
+    rest.get(htmlfile).on('complete', function(result) {
+      if (result instanceof Error) {
+        // Return empty result if URL not available.
+        callback("");
+      } else {
+        callback(cheerio.load(result));
+      }
+    });
+  } else {
+    callback(cheerio.load(fs.readFileSync(htmlfile)));
+  }
 };
 
 var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
+  return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+var checkHtmlFile = function(htmlfile, checksfile, callback) {
+  cheerioHtmlFile(htmlfile, function(result) {
+    // First check if we got a result
+    if (result !== "") {
+      var checks = loadChecks(checksfile).sort();
+      var out = {};
+
+      for (var ii in checks) {
+        var present = result(checks[ii]).length > 0;
         out[checks[ii]] = present;
+      }
+
+      // Return the results
+      callback(out);
+    } else {
+      callback();
     }
-    return out;
+  });
 };
 
 var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
-    return fn.bind({});
+  // Workaround for commander.js issue.
+  // http://stackoverflow.com/a/6772648
+  return fn.bind({});
 };
 
-if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+if (require.main == module) {
+  program
+    .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+    .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+    .option('-u, --url <html_file>', 'URL of index.html')
+    .parse(process.argv);
+
+  if (program.file) { // Call to asynchronous checkHtmlFile which starts the checking process.
+    checkHtmlFile(program.file, program.checks, function(result) {
+      var outJson = JSON.stringify(result, null, 4);
+      console.log(outJson);
+
+      process.exit(1);
+    });
+  } else {
+    assertURLExists(program.url, function(error) {
+      if (error == "error") {
+        console.log("%s does not exist. Exiting.", program.url);
+        process.exit(1);
+      } else {
+        // Call to asynchronous checkHtmlFile which starts the checking process.
+        checkHtmlFile(program.url, program.checks, function(result) {
+          var outJson = JSON.stringify(result, null, 4);
+          console.log(outJson);
+
+          process.exit(1);
+        });
+      }
+    });
+  }
+
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+  exports.checkHtmlFile = checkHtmlFile;
 }
